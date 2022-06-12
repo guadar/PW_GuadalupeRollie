3e4ab3769124a4841ea3930d62e2395e25c46182
guadar <rolliebg@gmail.com>
2022-06-11 22:34:12 -0300

    estructura y estilos barra superior

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { LogoArgProgComponent } from './componentes/logo-arg-prog/logo-arg-prog.component';
+import { RedesSocialesComponent } from './componentes/redes-sociales/redes-sociales.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { LoginComponent } from './componentes/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncabezadoComponent,
+    LogoArgProgComponent,
+    RedesSocialesComponent,
+    BannerComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
